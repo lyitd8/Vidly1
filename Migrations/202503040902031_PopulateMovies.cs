955b077235a53e44222b524049748c4541b29ca1
lyitd8 <kaixin.yap@bcssoftsolution.com>
2025-03-04 17:16:44 +0800

    Finish insert movies and genres

--- Migrations/202503040902031_PopulateMovies.cs
+++ Migrations/202503040902031_PopulateMovies.cs
@@ -7,6 +7,12 @@
     {
         public override void Up()
         {
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('Hangover', 1)");
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('Die Hard', 2)");
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('The Terminator', 3)");
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('Toy Story', 4)");
+            Sql("INSERT INTO Movies (Name, GenreId) VALUES ('Titanic', 5)");
+
         }
         
         public override void Down()
--- Models/Movie.cs
+++ Models/Movie.cs
@@ -9,6 +9,6 @@ namespace Vidly1.Models
 	{
 		public int Id { get; set; }
 		public string Name { get; set; }
-		public string Genre { get; set; }
+		public int GenreId { get; set; }
     }
 }
