4ed12578f75a8ffbd21233e87100bfda6434c964
lyitd8 <kaixin.yap@bcssoftsolution.com>
2025-03-04 12:21:58 +0800

    Modify Customer data from hardcode to database

--- Controllers/CustomersController.cs
+++ Controllers/CustomersController.cs
@@ -9,17 +9,29 @@ namespace Vidly1.Controllers
 {
     public class CustomersController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            _context.Dispose();
+        }
+
         // GET: Customers
         public ViewResult Index() //concrete class
         {
-            var customers = GetCustomers();
+            var customers = _context.Customers.ToList();
 
             return View(customers);
         }
 
         public ActionResult Details(int id) //abstract class
         {
-            var customer = GetCustomers().SingleOrDefault(c => c.Id == id);
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
 
             if (customer == null)
                 return HttpNotFound();
@@ -27,13 +39,6 @@ namespace Vidly1.Controllers
             return View(customer);
         }
 
-        private IEnumerable<Customer> GetCustomers()
-        {
-            return new List<Customer>
-            {
-                new Customer {Id = 1, Name = "Cillian Murphy"},
-                new Customer {Id = 2, Name = "Leopard"}
-            };
-        }
+       
     }
 }
