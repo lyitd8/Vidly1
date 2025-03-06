78642e7580f44e236f28405d628ed3126f2e38a8
lyitd8 <kaixin.yap@bcssoftsolution.com>
2025-03-06 16:45:00 +0800

    Create API for Customers

--- Global.asax.cs
+++ Global.asax.cs
@@ -5,6 +5,8 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Http;
+using Vidly1.App_Start;
 
 namespace Vidly1
 {
@@ -12,6 +14,7 @@ namespace Vidly1
     {
         protected void Application_Start()
         {
+            GlobalConfiguration.Configure(WebApiConfig.Register);
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
